ddee4a68c714cbaf3f112205a1eb328a17145828
Rex di Bona <rexdibona@users.noreply.github.com>
2018-10-29 12:32:21 +1100

    Added Trigger on Event infrastructure. This will allow commands to be started based on event status. Note we cannot run whileFalse as that doesn't really make sense. If necessary override the class with a new class that reverses the state of the event.

--- src/RobotRunner.hpp
+++ src/RobotRunner.hpp
@@ -11,15 +11,26 @@
  */
 #ifndef _ROBOTRUNNER_HPP
 #define _ROBOTRUNNER_HPP
-#include <RobotCmd.hpp>		// we need the RobotCmd strustures that we utilise.
+#include <RobotCmd.hpp>		// we need the RobotCmd structures that we utilise.
 
 class RobotRunner {
 public:
-	RobotCmd *_base;
+	RobotCmd *_base;	// normal command structures
+	RobotCmd *_adhoc;	// adhoc commands, just added as a command.
 	
 	RobotRunner::RobotRunner()
 	{
 		_base = NULL;	// initialise it!
+		_adhoc = TriggerMonitor::Instance();
+	}
+
+	RobotRunner *
+	RobotRunner::adhoc(RobotCmd *newRC) {
+		if (newRC == NULL) return;	// all done
+		_adhoc = newRC->setNext(_adhoc);
+		add(newRC->_sibling);	// should always be NULL
+		add(newRC->_child);	// should always be NULL
+		return this;
 	}
 
 	RobotRunner *
@@ -45,6 +56,16 @@ public:
 		RobotCmd *ptr;
 
 		for (ptr = _base; ptr != NULL; ptr = ptr->_next) {
+			if (ptr->_active == false) {
+				continue;
+			}
+			if (ptr->execute()) {
+				ptr->end();
+				ptr->_active = false;
+				setParallelActive(ptr->_child);
+			}
+		}
+		for (ptr = _adhoc; ptr != NULL; ptr = ptr->_next) {
 			if (ptr->_active == false) {
 				continue;
 			}
